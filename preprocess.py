9cd932723cef374fd8ef575b8f55e17c1be4e252
George Hotz <george@comma.ai>
2022-05-13 14:23:30 -0700

    ugh if you are watching i forget model.eval for the batchnorms

---
+++ preprocess.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import os
+import functools
+import torchaudio
+import csv
+from tqdm.auto import tqdm
+import torch
+
+CHARSET = " abcdefghijklmnopqrstuvwxyz,."
+DATASET = "/raid/ljspeech/LJSpeech-1.1"
+SAMPLE_RATE = 22050
+XMAX = 870    # about 10 seconds
+YMAX = 150
+
+def to_text(x):
+  return ''.join([CHARSET[c-1] for c in x if c != 0])
+
+@functools.lru_cache(None)
+def get_metadata():
+  ret = []
+  with open(os.path.join(DATASET, 'metadata.csv'), newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter='|')
+    for row in reader:
+      answer = [CHARSET.index(c)+1 for c in row[1].lower() if c in CHARSET]
+      if len(answer) <= YMAX:
+        ret.append((os.path.join(DATASET, 'wavs', row[0]+".wav"), answer))
+  return ret
+
+mel_transform = torchaudio.transforms.MelSpectrogram(SAMPLE_RATE, n_fft=1024, win_length=1024, hop_length=256, n_mels=80)
+def load_example(x):
+  waveform, sample_rate = torchaudio.load(x, normalize=True)
+  assert(sample_rate == SAMPLE_RATE)
+  mel_specgram = mel_transform(waveform)
+  #return 10*torch.log10(mel_specgram[0]).T
+  return mel_specgram[0].T
+
+if __name__ == "__main__":
+  meta = get_metadata() #[0:1000]
+  ex_x, ex_y = [], []
+  for x,y in tqdm(meta):
+    ex = load_example(x)
+    ex_x.append(ex)
+    ex_y.append((x, ex.shape[0], y))
+
+  sequences_padded = torch.nn.utils.rnn.pad_sequence(ex_x, batch_first=False) #.type(torch.float16)
+  print(sequences_padded.shape, sequences_padded.dtype)
+  print(ex_y[0])
+  torch.save(sequences_padded, "data/lj_x.pt")
+  torch.save(ex_y, "data/lj_y.pt")
--- rec.py
+++ rec.py
@@ -3,66 +3,28 @@ import os
 import time
 import csv
 import torch
+import random
 from tqdm.auto import tqdm
 from torch import log_softmax, nn
 import torch.optim as optim
-import torchaudio
 import numpy as np
 from torch.utils.data import Dataset
+from preprocess import load_example, to_text, CHARSET
 
-DATASET = "/raid/ljspeech/LJSpeech-1.1"
-CHARSET = " abcdefghijklmnopqrstuvwxyz,."
-XMAX = 870    # about 10 seconds
-YMAX = 150
-SAMPLE_RATE = 22050
+print("loading data X")
+ex_x = torch.load('data/lj_x.pt') #, map_location="cuda:0")
+print("copying to GPU", ex_x.shape)
+ex_x = ex_x.to(device="cuda:0", non_blocking=True)
+print("loading data Y")
+ex_y = torch.load('data/lj_y.pt')
+print("data loaded")
 
-import functools
-@functools.lru_cache(None)
-def get_metadata():
-  ret = []
-  with open(os.path.join(DATASET, 'metadata.csv'), newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter='|')
-    for row in reader:
-      answer = [CHARSET.index(c)+1 for c in row[1].lower() if c in CHARSET]
-      if len(answer) <= YMAX:
-        ret.append((os.path.join(DATASET, 'wavs', row[0]+".wav"), answer))
-  return ret
-
-mel_transform = torchaudio.transforms.MelSpectrogram(SAMPLE_RATE, n_fft=1024, win_length=1024, hop_length=256, n_mels=80)
-def load_example(x):
-  waveform, sample_rate = torchaudio.load(x, normalize=True)
-  assert(sample_rate == SAMPLE_RATE)
-  mel_specgram = mel_transform(waveform)
-  #return 10*torch.log10(mel_specgram[0]).T
-  return mel_specgram[0].T
-
-cache = {}
-def init_data():
-  meta = get_metadata()
-  for x,y in tqdm(meta):
-    cache[x] = load_example(x), y
-init_data()
-
-import hashlib
-class LJSpeech(Dataset):
-  def __init__(self, val=False):
-    self.meta = get_metadata()
-    if val:
-      cmp = lambda x: hashlib.sha1(x[0].encode('utf-8')).hexdigest()[0] == '0'
-    else:
-      cmp = lambda x: hashlib.sha1(x[0].encode('utf-8')).hexdigest()[0] != '0'
-    self.meta = [x for x in self.meta if cmp(x)]
-    print(f"set has {len(self.meta)}")
-
-  def __len__(self):
-    return len(self.meta)
-
-  def __getitem__(self, idx):
-    x,y = self.meta[idx]
-    if x not in cache:
-      print("NEVER SHOULD HAPPEN")
-      cache[x] = load_example(x), y
-    return cache[x]
+def get_sample(samples):
+  input = ex_x[:, samples]
+  input_lengths = [ex_y[i][1] for i in samples]
+  target = sum([ex_y[i][2] for i in samples], [])
+  target_lengths = [len(ex_y[i][2]) for i in samples]
+  return input, target, input_lengths, target_lengths
 
 class TemporalBatchNorm(nn.Module):
   def __init__(self, channels):
@@ -70,14 +32,8 @@ class TemporalBatchNorm(nn.Module):
     self.bn = nn.BatchNorm1d(channels)
 
   def forward(self, x):
-    # (L, N, C)
-    xx = x.permute(1,2,0)
-    # (N, C, L)
-    xx = self.bn(xx)
-    xx = xx.permute(2,0,1)
-    #print(x.shape, xx.shape)
-    #return self.bn(x.permute(1,2,0)).permute(1,2,0)
-    return xx
+    # (L, N, C) -> (N, C, L) -> (L, N, C)
+    return self.bn(x.permute(1,2,0)).permute(2,0,1)
 
 class Rec(nn.Module):
   def __init__(self):
@@ -105,100 +61,83 @@ class Rec(nn.Module):
     x = self.decode(x)
     return torch.nn.functional.log_softmax(x, dim=2)
 
-def pad_sequence(batch):
-  sorted_batch = sorted(batch, key=lambda x: x[0].shape[0], reverse=True)
-  input_lengths = [x[0].shape[0] for x in sorted_batch]
-  #input_lengths = [sorted_batch[0][0].shape[0] for x in sorted_batch]
-  target_lengths = [len(x[1]) for x in sorted_batch]
-  sequences = [x[0] for x in sorted_batch]
-  sequences_padded = torch.nn.utils.rnn.pad_sequence(sequences, batch_first=False)
-  labels = sum([x[1] for x in sorted_batch], [])
-  labels = torch.tensor(labels, dtype=torch.int32)
-  #labels = [x[1]+[0]*(YMAX - len(x[1])) for x in sorted_batch]
-  #labels = torch.LongTensor(labels)
-  #labels = labels[:, :max(target_lengths)]
-  return sequences_padded, labels, input_lengths, target_lengths
+WAN = False
+if WAN:
+  import wandb
 
-def get_dataloader(batch_size, val):
-  dset = LJSpeech(val)
-  trainloader = torch.utils.data.DataLoader(dset, batch_size=batch_size, shuffle=True, num_workers=2 if val else 8, collate_fn=pad_sequence, pin_memory=True)
-  return dset, trainloader
-
-import wandb
-
-WAN = True
 def train():
+  epochs = 300
+  learning_rate = 0.001 #3e-4
+  batch_size = 128
+
   if WAN:
     wandb.init(project="tinyvoice", entity="geohot")
-
-  epochs = 300
-  learning_rate = 3e-4
-  batch_size = 128
-  wandb.config = {
-    "learning_rate": learning_rate,
-    "epochs": epochs,
-    "batch_size": batch_size
-  }
+    wandb.config = {
+      "learning_rate": learning_rate,
+      "epochs": epochs,
+      "batch_size": batch_size
+    }
 
   timestamp = int(time.time())
-  dset, trainloader = get_dataloader(batch_size, False)
-  valdset, valloader = get_dataloader(batch_size, True)
   ctc_loss = nn.CTCLoss().cuda()
   model = Rec().cuda()
-  model.load_state_dict(torch.load('models/tinyvoice_1652472269_7.pt'))
+  model.load_state_dict(torch.load('models/tinyvoice_1652472897_98.pt'))
 
-  #optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+  #optimizer = optim.Adam(model.parameters(), lr=learning_rate)
   import apex
   optimizer = apex.optimizers.FusedAdam(model.parameters(), lr=learning_rate)
-  #optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+  split = int(ex_x.shape[1]*0.9)
+  trains = [x for x in range(split)]
+  vals = [x for x in range(split, ex_x.shape[1])]
+  val_batches = np.array(vals)[:len(vals)//batch_size * batch_size].reshape(-1, batch_size)
+
   val = torch.tensor(load_example('data/LJ037-0171.wav')).cuda()
   for epoch in range(epochs):
     if WAN:
       wandb.watch(model)
 
-    if epoch%2 == 0:
-      mguess = model(val[:, None])
-      pp = ''.join([CHARSET[c-1] for c in mguess[:, 0, :].argmax(dim=1).cpu() if c != 0])
-      print("VALIDATION", pp)
-      torch.save(model.state_dict(), f"models/tinyvoice_{timestamp}_{epoch}.pt")
+    with torch.no_grad():
+      model.eval()
 
-    t = tqdm(valloader, total=len(valdset)//batch_size)
-    losses = []
-    for data in t:
-      input, target, input_lengths, target_lengths = data
-      input = input.to('cuda:0', non_blocking=True)
-      target = target.to('cuda:0', non_blocking=True)
-      guess = model(input)
-      loss = ctc_loss(guess, target, input_lengths, target_lengths)
-      losses.append(loss)
-    val_loss = torch.mean(torch.tensor(losses)).item()
-    print(f"val_loss: {val_loss:.2f}")
+      mguess = model(val[:, None])
+      pp = to_text(mguess[:, 0, :].argmax(dim=1).cpu())
+      print("VALIDATION", pp)
+      if epoch%5 == 0 and epoch != 0:
+        torch.save(model.state_dict(), f"models/tinyvoice_{timestamp}_{epoch}.pt")
+
+      losses = []
+      for samples in (t:=tqdm(val_batches)):
+        input, target, input_lengths, target_lengths = get_sample(samples)
+        target = torch.tensor(target, dtype=torch.int32, device='cuda:0')
+        guess = model(input)
+        loss = ctc_loss(guess, target, input_lengths, target_lengths)
+        print(loss)
+        losses.append(loss)
+      val_loss = torch.mean(torch.tensor(losses)).item()
+      print(f"val_loss: {val_loss:.2f}")
+
     if WAN:
       wandb.log({"val_loss": val_loss})
 
-    t = tqdm(trainloader, total=len(dset)//batch_size)
-    for data in t:
-      input, target, input_lengths, target_lengths = data
-      input = input.to('cuda:0', non_blocking=True)
-      target = target.to('cuda:0', non_blocking=True)
+    random.shuffle(trains)
+    model.train()
+    batches = np.array(trains)[:len(trains)//batch_size * batch_size].reshape(-1, batch_size)
+    for samples in (t:=tqdm(batches)):
+      input, target, input_lengths, target_lengths = get_sample(samples)
+      target = torch.tensor(target, dtype=torch.int32, device='cuda:0')
+
       optimizer.zero_grad()
       guess = model(input)
-      #print(input)
-      #print(guess)
-      #print(target)
-      #print(guess.shape, target.shape, input_lengths, target_lengths)
 
-      """
-      pp = ''.join([CHARSET[c-1] for c in guess[:, 0, :].argmax(dim=1).cpu() if c != 0])
+      pp = to_text(guess[:, 0, :].argmax(dim=1).cpu())
       if len(pp) > 0:
         print(pp)
-      """
 
       loss = ctc_loss(guess, target, input_lengths, target_lengths)
-      #print(loss)
-      #loss = loss.mean()
       loss.backward()
       optimizer.step()
+
       t.set_description("loss: %.2f" % loss.item())
       if WAN:
         wandb.log({"loss": loss})
