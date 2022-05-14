ec3ff4fe7f4a58d93f3dc5e39ae733df8f95472f
Jacky Lee <jla524@sfu.ca>
2022-05-13 23:04:38 -0700

    Fix indentation

--- autoencode.py
+++ autoencode.py
@@ -8,41 +8,43 @@ from torchvision import transforms
 import pytorch_lightning as pl
 
 class LitAutoEncoder(pl.LightningModule):
-	def __init__(self):
-		super().__init__()
-		self.encoder = nn.Sequential(
+  def __init__(self):
+    super().__init__()
+    self.encoder = nn.Sequential(
       nn.Linear(28 * 28, 64),
       nn.ReLU(),
-      nn.Linear(64, 3))
-		self.decoder = nn.Sequential(
+      nn.Linear(64, 3)
+    )
+    self.decoder = nn.Sequential(
       nn.Linear(3, 64),
       nn.ReLU(),
-      nn.Linear(64, 28 * 28))
+      nn.Linear(64, 28 * 28)
+    )
 
-	def forward(self, x):
-		embedding = self.encoder(x)
-		return embedding
+  def forward(self, x):
+    embedding = self.encoder(x)
+    return embedding
 
-	def configure_optimizers(self):
-		optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-		return optimizer
+  def configure_optimizers(self):
+    optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+    return optimizer
 
-	def training_step(self, train_batch, batch_idx):
-		x, y = train_batch
-		x = x.view(x.size(0), -1)
-		z = self.encoder(x)    
-		x_hat = self.decoder(z)
-		loss = F.mse_loss(x_hat, x)
-		self.log('train_loss', loss)
-		return loss
+  def training_step(self, train_batch, batch_idx):
+    x, y = train_batch
+    x = x.view(x.size(0), -1)
+    z = self.encoder(x)
+    x_hat = self.decoder(z)
+    loss = F.mse_loss(x_hat, x)
+    self.log('train_loss', loss)
+    return loss
 
-	def validation_step(self, val_batch, batch_idx):
-		x, y = val_batch
-		x = x.view(x.size(0), -1)
-		z = self.encoder(x)
-		x_hat = self.decoder(z)
-		loss = F.mse_loss(x_hat, x)
-		self.log('val_loss', loss)
+  def validation_step(self, val_batch, batch_idx):
+    x, y = val_batch
+    x = x.view(x.size(0), -1)
+    z = self.encoder(x)
+    x_hat = self.decoder(z)
+    loss = F.mse_loss(x_hat, x)
+    self.log('val_loss', loss)
 
 # data
 dataset = MNIST('', train=True, download=True, transform=transforms.ToTensor())
